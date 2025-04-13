0375f9378911c68ea3551e4c17e92ee9403e3411
jet-c-21@puff-nobel-numbat <jet-c-21@puff-nobel-numbat>
2025-04-13 21:08:53 +0800

    fix

--- scripts/apply_personal_gsettings.sh
+++ scripts/apply_personal_gsettings.sh
@@ -135,7 +135,7 @@ change_dock_to_macos_style() {
 
   gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
   gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
-  gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'BOTTOM'
+  # gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'BOTTOM'
   gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top true
   gsettings set org.gnome.shell.extensions.dash-to-dock icon-size-fixed false
 

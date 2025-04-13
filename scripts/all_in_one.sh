4ab6c4e5278b15ca67be152eae1c2940c265e3e0
jet-c-21@puff-nobel-numbat <jet-c-21@puff-nobel-numbat>
2025-04-13 19:15:52 +0800

    fix

--- scripts/all_in_one.sh
+++ scripts/all_in_one.sh
@@ -363,7 +363,7 @@ install_gstreamer() {
   # Install GStreamer and its plugins
   sudo apt install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio
 
-  cl_print "[*INFO*] - Finished installing GStreamer \n"
+  cl_print "[*INFO*] - Finished installing GStreamer \n" "green"
 }
 
 install_extra_codec() {

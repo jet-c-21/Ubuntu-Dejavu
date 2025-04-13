c5293ca80ffebb3b25d739ba067302a04307c5cc
jet-c-21@puff-nobel-numbat <jet-c-21@puff-nobel-numbat>
2025-04-13 19:45:58 +0800

    fix

--- scripts/install_flatpak.sh
+++ scripts/install_flatpak.sh
@@ -70,7 +70,7 @@ install_flatpak() {
 
   sudo apt install -y flatpak
   sudo apt install -y gnome-software-plugin-flatpak
-  flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+  sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
   cl_print "[*INFO*] - finished installing flatpak \n"
 }

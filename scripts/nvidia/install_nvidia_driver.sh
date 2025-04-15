dc6fdeb1fcb13437db4eeab39981b8933f8ccaa5
jet-c-21 <67193495+jet-c-21@users.noreply.github.com>
2025-04-15 11:51:37 +0800

    Add install_nvidia_driver.sh script for NVIDIA driver installation

---
+++ scripts/nvidia/install_nvidia_driver.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+set -e
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>> color print >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+declare -A COLORS=(
+  ["red"]='\e[1;31m'
+  ["green"]='\e[1;32m'
+  ["blue"]='\e[1;34m'
+  ["yellow"]='\e[1;33m'
+  ["magenta"]='\e[1;35m'
+  ["cyan"]='\e[1;36m'
+  ["pink"]='\e[1;38;5;206m'
+  ["white"]='\e[1;37m'
+)
+COLOR_RESET='\e[0m'
+
+cl_print() {
+  local text=$1
+  local color=$2
+  if [ "$color" == "dflt" ]; then
+    echo -e "$text"
+    return
+  fi
+  if [ $# -eq 1 ] || [ -z "${COLORS[$color]}" ]; then
+    color="pink"
+  fi
+  echo -e "${COLORS[$color]}${text}${COLOR_RESET}"
+}
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<< color print <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ask user input sudo password >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+if [[ -z "${SUDO_PASSWORD}" ]]; then
+  echo -n "[*INFO*] - Please enter your sudo password: "
+  read -s user_input_password
+  echo
+  verify_password() {
+    echo "$1" | sudo -S -v &>/dev/null
+  }
+  if verify_password "$user_input_password"; then
+    export SUDO_PASSWORD="$user_input_password"
+    echo "$SUDO_PASSWORD" | sudo -S -v &>/dev/null
+  else
+    echo "[*ERROR*] - Incorrect password." >&2
+    exit 1
+  fi
+fi
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<< ask user input sudo password <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>> use and unlock sudo >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+use_sudo() {
+  local cmd="echo ${SUDO_PASSWORD} | sudo -SE "
+  for param in "$@"; do
+    cmd+="${param} "
+  done
+  eval "${cmd}"
+}
+
+unlock_sudo() {
+  local result="$(use_sudo whoami)"
+  cl_print "[*INFO*] - unlocked sudo for user: $result" "cyan"
+}
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<< use and unlock sudo <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+install_flatpak() {
+  cl_print "[*INFO*] - start installing flatpak ..."
+  unlock_sudo
+
+  sudo apt install -y flatpak
+  sudo apt install -y gnome-software-plugin-flatpak
+  sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+  cl_print "[*INFO*] - finished installing flatpak \n" "green"
+}
+
+main() {
+  install_flatpak
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  main "$@"
+fi
